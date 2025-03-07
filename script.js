8ae72ee094cf469fc6545d9f653496a69f08410e
ashish <ashishmasih92g@gmail.com>
2025-03-07 13:20:43 +0530

    new assignment completed

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+
+let input = document.querySelector("form")
+
+
+input.addEventListener("dblclick",()=>{
+   
+    let select = document.querySelector("#colorSelect")
+    if(select.selectedIndex==-1){
+    alert("you haven't selected yet")
+        return
+    }
+    
+    select.remove(select.selectedIndex)
+})
+
